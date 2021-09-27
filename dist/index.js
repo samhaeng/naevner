0f94e6d95aede5e61a32d9bdf6475e82e4553584
Rasmus Wølk <rasmuswoelk@gmail.com>
2021-09-27 11:46:53 +0200

    Bumped and published version. Deleted dist folder.

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./naevner.js.cjs.production.min.js')
-} else {
-  module.exports = require('./naevner.js.cjs.development.js')
-}
